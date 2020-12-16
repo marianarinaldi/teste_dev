d846b10241b24bba68b33ed19c61d0ea026ca2fb
Mariana Rinaldi de Souza <rinaldi.mari@gmail.com>
2020-12-15 21:16:37 -0300

    first commit

---
+++ database/migrations/2020_12_15_233850_create_enrollments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnrollmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('courses_id')->unsigned();
+            $table->integer('students_id')->unsigned();
+            $table->foreign('courses_id')
+                ->references('id')->on('courses')
+                ->onDelete('cascade');
+            $table->foreign('students_id')
+                ->references('id')->on('students')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrollments');
+    }
+}
